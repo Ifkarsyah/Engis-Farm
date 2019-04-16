1d5ffc5dbcaecdb1439370fce541ad9a147051be
ifkarsyah <ferdian.ifkarsyah@gmail.com>
2019-04-16 10:31:37 +0700

    add sample docs in player

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -10,42 +10,81 @@ import java.awt.*;
 import java.util.Arrays;
 import java.util.LinkedList;
 
+/**
+ * Represent Player
+ *
+ * @author ifkarsyah
+ */
 public class Player extends Renderable {
     private Point point;
     private int water = 20;
     private int money = 0;
     private LinkedList<Product> inventory;
 
+    /**
+     * Instantiate Player with initial position and empty inventory
+     * @param point initial position
+     */
     public Player(Point point) {
         this.point = point;
         this.inventory = new LinkedList<>();
     }
 
+    /**
+     * Get player's point
+     * @return player's current point state
+     */
     public Point getPoint() {
         return point;
     }
 
+    /**
+     * Get player's inventory
+     * @return player's current inventory state
+     */
     public LinkedList<Product> getInventory() {
         return inventory;
     }
 
+    /**
+     * Get player's representation in map
+     * @return representation of player in map
+     */
     @Override
     public char render() {
         return 'p';
     }
 
+    /**
+     * Refill player's water when interacting with well
+     * I.S: Random (TODO: what 'sembarang' means in English? Replace with it!
+     * F.S: player's water added 100
+     * @param well the well player's interacting with
+     */
     public void interact(Well well) {
         water += well.waterCapacity;
         if (water > 100)
             water = 100;
     }
 
+    /**
+     * Clear inventory by selling all product to truck
+     * I.S: Random
+     * F.S: player's inventory empty, truck's inventory is empty
+     * @param truck the truck player's interacting with
+     */
     public void interact(Truck truck) {
         while (!inventory.isEmpty())
             money += inventory.removeFirst().price;
         truck.resetCooldownTime();
     }
 
+    /**
+     * Get product other than meat from animal
+     * I.S: there's farmAnimal around player
+     * F.S: can't get animal's product within sort time
+     * @param farmAnimal the animal player's interacting with
+     */
     public void interact(FarmAnimal farmAnimal) {
         try {
             if (farmAnimal.isProductReady) {
@@ -59,6 +98,12 @@ public class Player extends Renderable {
         }
     }
 
+    /**
+     * Get meat from animal
+     * I.S: there's farmAnimal around player
+     * F.S: can't get animal's meat within sort time
+     * @param farmAnimal the animal player's interacting with
+     */
     public void kill(FarmAnimal farmAnimal) {
         try {
             if (farmAnimal.isProductReady) {
@@ -72,15 +117,29 @@ public class Player extends Renderable {
         }
     }
 
+    /**
+     * Grow land in the same point with Player
+     * I.S: Random
+     * F.S: player's water reduced by 1 and land's grass is grew
+     * @param land the land Player standing
+     */
     public void grow(Land land) {
         land.isGrassGrown = true;
         water--;
     }
 
+    /**
+     * Trigger animal to sound
+     * @param farmAnimal targeted animal
+     */
     public void talk(FarmAnimal farmAnimal) {
         farmAnimal.makeSound();
     }
 
+    /**
+     * Print player's info
+     * @return player's info
+     */
     @Override
     public String toString() {
         return "" + ("Money: " + money + "\n") + ("Water: " + water + "\n") + ("Inventory: " + Arrays.toString(inventory.toArray()) + "\n");
