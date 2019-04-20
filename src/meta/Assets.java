ddb4946fae86cfa633d50f98d880168bf799ea66
Juniardi Akbar <juniardiakbar@gmail.com>
2019-04-20 23:30:53 +0700

    Assets.java

---
+++ src/meta/Assets.java
@@ -0,0 +1,32 @@
+package meta;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+
+/**
+ * Kelas Assets yang merepresentasikan pengambilan gambar dari file png
+ */
+
+public class Assets {
+    public static Image Ayam;
+    public static Image Bebek;
+    public static Image Domba;
+    public static Image Kambing;
+    public static Image Sapi;
+    public static Image Unta;
+
+    public void init(){
+        try{
+            Ayam = ImageIO.read(getClass().getResource("img/"));
+            Bebek = ImageIO.read(getClass().getResource("img/"));
+            Domba = ImageIO.read(getClass().getResource("img/"));
+            Kambing = ImageIO.read(getClass().getResource("img/"));
+            Sapi = ImageIO.read(getClass().getResource("img/"));
+            Unta = ImageIO.read(getClass().getResource("img/"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
