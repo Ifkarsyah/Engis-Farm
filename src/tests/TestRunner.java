a922e9cf0dec277ef3d8bcb8060a27dc857e09f9
Juniardi Akbar <juniardiakbar@gmail.com>
2019-04-20 23:52:24 +0700

    fix

--- src/tests/TestRunner.java
+++ src/tests/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.notification.Failure;
 
 public class TestRunner {
   public static void main(String[] args) {
-    Result result = JUnitCore.runClasses(tests.productTest.class);
+    Result result = JUnitCore.runClasses(tests.product_test.productTest.class);
     for (Failure failure : result.getFailures()) {
       System.out.println(failure.toString());
     }
