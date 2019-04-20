e2490617cc5fe7b6905a0a4cbff793a4c2e611d5
Juniardi Akbar <juniardiakbar@gmail.com>
2019-04-21 02:39:05 +0700

    start menu

--- src/UI/Launcher.java
+++ src/UI/Launcher.java
@@ -6,10 +6,19 @@ import game.Game;
 import javax.swing.*;
 import java.awt.*;
 
-class Launcher {
+class Launcher extends JFrame {
     private static final Game game = new Game();
 
     public static void main(String[] args) {
+        try {
+            Launcher frame = new Launcher();
+            frame.setVisible(true);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Launcher(){
         javax.swing.SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Engi's Farm");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
