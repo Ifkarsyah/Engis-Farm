b2a35111fcf17e5a9861479ec5c0da6c4327d891
ifkarsyah <ferdian.ifkarsyah@gmail.com>
2019-04-21 07:30:09 +0700

    add sample junit

---
+++ src/entities/animals/AyamTest.java
@@ -0,0 +1,72 @@
+package entities.animals;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AyamTest {
+
+    private Object ayam;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        Ayam ayam = new Ayam("pablo");
+
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void becomeHungrier() {
+        setUp();
+        assertEquals(2-1, 1);
+    }
+
+    @org.junit.jupiter.api.Test
+    void isDeath() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void eat() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProduct() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAnimalMeat() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getHungryPoint() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void render() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void renderGUI() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProduct1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAnimalMeat1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void makeSound() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void render1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void renderGUI1() {
+    }
+}
